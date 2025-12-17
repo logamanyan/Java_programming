66ec37b4591d562c6d074e58ee4e0d5409a4d3a8
Logamanyan R <146450819+logamanyan@users.noreply.github.com>
2025-12-17 14:17:05 +0530

    Rename class from Fahernheat to Fahrenheit

--- Operators/Formula Based/Fahrenheit.java
+++ Operators/Formula Based/Fahrenheit.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Fahernheat {
+public class Fahrenheit {
     public static void main(String[] args)
     {
         Scanner s=new Scanner(System.in);
@@ -8,3 +8,4 @@ public class Fahernheat {
         System.out.printf("%.2f",c);
     }
 }
+
